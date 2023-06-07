9b55755e8de818ba3c0e1fb817c26deccc9b350e
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-06-07 19:24:51 +0200

    setting review forms

---
+++ reviews/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import ReviewForm
+
+
+def review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thank_you')
+
+    else:
+        form = ReviewForm()
+
+    return render(request, 'reviews/review.html', {
+        'form': form,
+    })
+
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
